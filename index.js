7ec9f680a1b296ab9aa62235019e339055fc3f5e
Timur <timur.begaliev@gmail.com>
2016-03-11 00:09:35 -0500

    config vars added

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_7m15mn5p:221b4pkgonqghgn778vkpjcg3d@ds011439.mlab.com:11439/heroku_7m15mn5p',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337'  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || '2jV4pb35y2u5N7e2R5Jnj8pBWUQRIo52AQHJnL92',
+  masterKey: process.env.MASTER_KEY || 'nFPRUOfP2fPU17bGnDsriep7ZIfNI6UsD3sTdQ65', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://instaviewerplus.herokuapp.com/api'  // Don't forget to change to https if needed
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
